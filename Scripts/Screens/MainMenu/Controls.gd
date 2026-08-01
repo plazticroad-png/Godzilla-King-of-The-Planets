6805a3711ced391b5273e1adbec101baedd86b44
plazticroad-png <310338305+plazticroad-png@users.noreply.github.com>
2026-08-01 21:49:57 +1000

    feat: reworked control system and added Xbox defaults
    The default input map as configured in project settings now includes
    keyboard, gamepad buttons and joystick directions.
    The user-configured input map now only overrides the default mapping of
    the same type. The other defaults will stay enabled alongside.

--- Scripts/Screens/MainMenu/Controls.gd
+++ Scripts/Screens/MainMenu/Controls.gd
@@ -102,30 +102,24 @@ func save_mapping() -> void:
 		file.set_value(SECTION, ACTIONS[i], mapping[i])
 	SaveManager.save_settings_file(file)
 	
-static func init_controls() -> void:
-	Input.joy_connection_changed.connect(func(_device: int, _connected: bool) -> void:
-		load_mapping(SaveManager.load_settings_file())
-		)
-	
 static func load_mapping(file: ConfigFile) -> void:
 	if not file.has_section("Input"):
 		return
-	var has_joypad: bool = Input.get_connected_joypads().size() > 0
 	
-	# Reset the input mapping
+	# Reset the input mapping to defaults
 	InputMap.load_from_project_settings()
 	
 	for action: String in ACTIONS:
 		var input: InputEvent = file.get_value("Input", action)
-		if not (input is InputEventKey) and not has_joypad:
-			continue
+		var input_type = input.get_class()
 		
-		# Get the default input events for this action that are not keys
-		# so we can load them again below
-		# For example, so we can have both a key and a gamepad stick movement
-		# as the player's move input action
+		# Construct the event list from defaults, excluding default
+		# events of the same type as the given config event.
+		# For example, if the config gives an InputEventKey, the
+		# default key event will not be used but the default joy events
+		# will still work.
 		var events := InputMap.action_get_events(action).filter(func(x: InputEvent) -> bool:
-			return not (x is InputEventKey)
+			return not x.is_class(input_type)
 			)
 		InputMap.action_erase_events(action)
 		InputMap.action_add_event(action, input)
